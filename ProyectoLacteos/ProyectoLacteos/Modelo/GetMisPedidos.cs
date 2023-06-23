41d662b95493377c0dd0161c9c5493255d38ec8a
Cristian Rivera <adbel.rivera@unitec.edu>
2023-06-22 18:52:47 -0600

    comit
    Mis pedidos muestra todo lo del web service de tbl pedido

---
+++ ProyectoLacteos/ProyectoLacteos/Modelo/GetMisPedidos.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProyectoLacteos.Modelo
+{
+    public class GetMisPedidos
+    {
+           public string id { get; set; }
+           public string id_usuario { get; set; }
+           public string fecha_pedido { get; set; }
+           public string id_direccion { get; set; }
+           public string estado { get; set; }
+        }
+
+    public class LinkMisPedidos
+    {
+        public string rel { get; set; }
+        public string href { get; set; }
+    }
+
+    public class GetMisPedidosResponse
+    {
+        public List<GetMisPedidos> items { get; set; }
+        public bool hasMore { get; set; }
+        public int limit { get; set; }
+        public int offset { get; set; }
+        public int count { get; set; }
+        public List<LinkMisPedidos> links { get; set; }
+    }
+
+}
